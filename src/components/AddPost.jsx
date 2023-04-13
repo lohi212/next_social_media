24f2a6a4573e64f2c5f9e62af9e5f5b7a7eec3d1
lohi212 <suvvarilohita@gmail.com>
2023-04-13 17:35:20 +0530

    feat(css): add a post

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,13 +1,17 @@
-import { Button, Modal, Input } from "antd";
-import React, { useState } from "react";
+import { Button, Modal, Input, Card } from "antd";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 import UploadWidget from "./UploadWidget";
 
 const AddPost = ({ handleAddPost }) => {
+  const inputRef = useRef(null);
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
   const [imagesUploadedList, setImagesUploadedList] = useState([]);
 
-  const handleAdd = () => {
+  const handleAdd = (e) => {
     setOpen(true);
+    if (inputRef.current) inputRef.current.blur();
   };
 
   const handleClose = () => {
@@ -15,16 +19,60 @@ const AddPost = ({ handleAddPost }) => {
     setImagesUploadedList([]);
   };
 
+  const handlePost = () => {
+    handleAddPost({
+      caption: value,
+      images: imagesUploadedList,
+      // likes,
+      // comments
+      id: Date.now(),
+    });
+    handleClose();
+  };
+
   return (
     <div>
-      <Button onClick={handleAdd}>Add Post</Button>
+      <Card
+        bodyStyle={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          alt="profile_photo"
+          className="rounded-[50%]"
+          src="https://imgur.com/ffMnKlq.png"
+          width={60}
+          height={60}
+        />
+        <Input
+          ref={inputRef}
+          value={value}
+          className="ant-input-search "
+          placeholder="Start a post"
+          onFocus={handleAdd}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </Card>
 
       <Modal
         title="Create post"
         open={open}
-        onOk={handleClose}
         onCancel={handleClose}
         width="80%"
+        okText="Post"
+        onOk={handlePost}
+        okButtonProps={{
+          disabled: !value,
+        }}
+        cancelButtonProps={{
+          style: {
+            display: "none",
+          },
+        }}
       >
         <p>Username</p>
         <Input.TextArea placeholder="Enter caption" className="mb-10" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import AddPost from "@/components/AddPost";
 
 function Home() {
+  const handleAddPost = () => {
+    //
+  };
   return (
     <main className="p-5 root-container">
       <h1 className="text-center">Social Media App</h1>
-      <AddPost />
+      <AddPost handleAddPost={handleAddPost} />
     </main>
   );
 }
